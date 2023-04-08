8d6e23ea42ec9c7bbfbefce4ce83f77c37ea14db
Richard Warren Hornbuckle <longhornbuckle@utexas.edu>
2023-04-07 21:38:11 -0500

    Update tensor_view.hpp

--- include/experimental/linear_algebra/tensor_view.hpp
+++ include/experimental/linear_algebra/tensor_view.hpp
@@ -16,9 +16,13 @@ namespace std
 namespace math
 {
   template < class MDS >
-  class vector_view;
+  class vector_view requires ( detail::is_mdspan_v<MDS> &&
+                              ( MDS::extents_type::rank()== 1 ) &&
+                              MDS::is_always_unique() );
   template < class MDS >
-  class matrix_view;
+  class matrix_view requires ( detail::is_mdspan_v<MDS> &&
+                              ( MDS::extents_type::rank()== 2 ) &&
+                              MDS::is_always_unique() );
 }
 }
 
